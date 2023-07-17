823519ef7b1dc0c268a836aa1861878633502914
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-07-17 11:12:27 +0530

    Add files via upload

---
+++ Stack/previous smaller element.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(2);
+    v.push_back(1);
+    v.push_back(4);
+    v.push_back(3);
+    stack<int> s;
+    vector<int> ans(v.size());
+    s.push(-1);
+    for (int i=0;i<v.size();i++)
+    {
+        int curr = v[i];
+        while (s.top() >= curr)
+        {
+            s.pop();
+        }
+        ans[i] = s.top();
+        s.push(curr);
+    }
+    // printing the ans
+    for (int j = 0; j< v.size(); j++)
+    {
+        cout << ans[j] << " ";
+    }
+}
